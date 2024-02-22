0b309d153aea09003ae3ee5c5fc787b96fb46387
Shivanshu Mishra <shivanshumishra@PAISAMAC082.local>
2024-02-22 12:19:13 +0530

    scroll view horizontal

---
+++ components/FlatCards.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text style = {styles.headingText}>Flat Cards</Text>
+        <View style = {styles.container}>
+                <View style = {[styles.card,styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+                <View style = {[styles.card,styles.cardTwo]}>
+                    <Text>Blue</Text>
+                </View>
+                <View style = {[styles.card,styles.cardThree]}>
+                    <Text>Green</Text>
+                </View>
+                <View style = {[styles.card,styles.cardThree]}>
+                    <Text>Redd</Text>
+                </View>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText : {
+        fontSize : 24,
+        fontWeight: 'bold',
+        paddingHorizontal : 10
+    },
+    container : {
+        flex : 1,
+        flexDirection : 'row',
+        padding : 8 
+    },
+    card : {
+        flex : 1,
+        width : '20%',
+        height : 100,
+        borderRadius : 4,
+        margin: 8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cardOne : {
+        backgroundColor : '#EF5354'
+    },
+    cardTwo : {
+        backgroundColor : '#5DA3FA'
+    },
+    cardThree : {
+        backgroundColor : '#50DBB4'
+    }
+})
